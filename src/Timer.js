288779bef2ddaf1646b11ba2df45aa6e75a8be5e
Samuel Thompson <thomps9012@gmail.com>
2020-11-16 11:17:11 -0500

    changed alert display

--- src/Timer.js
+++ src/Timer.js
@@ -39,7 +39,7 @@ export default class Timer extends Component {
         return(
             <div>
                 { minutes === 0 && seconds === 0
-                    ?<h1>Busted!</h1>
+                    ?<h1>Times Up!</h1>
                 : <h1>Time Remaining: {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1>
                 }
             </div>
