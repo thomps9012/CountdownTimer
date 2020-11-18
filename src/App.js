07d965e8651051cfabeda9a1f6ee5dfb40a8c37e
Samuel Thompson <thomps9012@gmail.com>
2020-11-18 09:17:43 -0500

    set up an image replace and final image when timer is complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.scss';
 import Timer from './Timer';
 import Credits from './Credits';
-import hourglass from './images/hourglass.gif';
+
 
 
 function App() {
   return (
     <div id="timer">
-      <img src={hourglass} alt="hourglass" />
       <Timer />
       <Credits />
     </div>
--- src/Timer.js
+++ src/Timer.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './Timer.scss';
+import confetti from './images/confetti.gif';
+import hourglass from './images/hourglass.gif';
 export default class Timer extends Component {
 
     state = {
@@ -7,41 +9,48 @@ export default class Timer extends Component {
         seconds: 0
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.myInterval = setInterval(() => {
             const { seconds, minutes } = this.state
-            
-            if (seconds > 0){
-            this.setState(({ seconds}) => ({
-                seconds: seconds -1
-            }))
-        }
-        
-        if (seconds === 0) {
-            if(minutes === 0) {
-                clearInterval(this.myInterval)
-            } else{
-                this.setState(({ minutes }) => ({
-                    minutes: minutes -1,
-                    seconds: 59
+
+            if (seconds > 0) {
+                this.setState(({ seconds }) => ({
+                    seconds: seconds - 1
                 }))
             }
-        }
-    }, 1000)
+
+            if (seconds === 0) {
+                if (minutes === 0) {
+                    clearInterval(this.myInterval)
+                } else {
+                    this.setState(({ minutes }) => ({
+                        minutes: minutes - 1,
+                        seconds: 59
+                    }))
+                }
+            }
+        }, 1000)
     }
 
-    componentWillUnmount(){
+    componentWillUnmount() {
         clearInterval(this.myInterval)
     }
 
     render() {
         const { minutes, seconds } = this.state
-        
-        return(
+
+        return (
             <div>
                 { minutes === 0 && seconds === 0
-                    ?<h1>Times Up!</h1>
-                : <h1>Time Remaining: {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1>
+                    ? <div>
+                        <img src={confetti} alt="confetti" />
+                        <h1>Times Up!</h1>
+                    </div>
+                    :
+                    <div>
+                        <img src={hourglass} alt="hourglass" />
+                        <h1>Time Remaining: {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1>
+                    </div>
                 }
             </div>
         )
